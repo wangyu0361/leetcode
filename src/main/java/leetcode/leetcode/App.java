a29ba12d074c841568bfc843d00941e98cf7176b
ray <wang@pacificcontrols.net>
2015-11-20 09:35:37 -0500

    test commmit

--- src/main/java/leetcode/leetcode/App.java
+++ src/main/java/leetcode/leetcode/App.java
@@ -1,5 +1,7 @@
 package leetcode.leetcode;
 
+import leetcode.leetcode.Library;
+
 /**
  * Hello world!
  *
@@ -8,6 +10,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+    	Library library = new Library();
+    	library.test();
     }
 }
---
+++ src/main/java/leetcode/leetcode/Library.java
@@ -0,0 +1,8 @@
+package leetcode.leetcode;
+
+public class Library{
+	public void test(){
+		System.out.println(123);
+	}
+	
+} 
