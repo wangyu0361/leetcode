71eb33d9737749f26d520e8235524d6eea68a4d0
ray <wang@pacificcontrols.net>
2015-11-23 17:52:40 -0500

    intersection of 2 linked list solution 1

--- src/main/java/leetcode/leetcode/Library.java
+++ src/main/java/leetcode/leetcode/Library.java
@@ -2,6 +2,15 @@ package leetcode.leetcode;
 
 public class Library{
 
+	public class ListNode {
+		int val;
+		ListNode next;
+		ListNode(int x) {
+			val = x;
+			next = null;
+		}
+	}
+
 	/***
 	 * quick sort algorithm to sort a char array, the idea is partition the array into 2 parts : left part less than an element ( randomly pick, usually the fist or last element)
 	 *  right part greater or equal to one element.
@@ -56,5 +65,60 @@ public class Library{
 	}
 
 
+	/***
+	 * Write a program to find the node at which the intersection of two singly linked lists begins.
+
+	For example, the following two linked lists:
+
+	A:		a1 → a2
+                                           ↘
+                 	c1 → c2 → c3
+                                           ↗            
+	B:b1 → b2 → b3
+
+	begin to intersect at node c1.
+
+	Notes:
+
+	If the two linked lists have no intersection at all, return null.
+	The linked lists must retain their original structure after the function returns.
+	You may assume there are no cycles anywhere in the entire linked structure.
+	Your code should preferably run in O(n) time and use only O(1) memory.
+	 * 
+	 * 
+	 */
+
+	public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+		if(headA==null || headB==null){
+			return null;
+		}
+		ListNode a = headA;
+		ListNode b = headB;
+
+		while(a!=b){
+			if(a==null){
+				a=headB;
+			}
+			else{
+				a=a.next;
+			}
+
+			if(b==null){
+				b=headA;
+			}
+			else{
+				b=b.next;
+			}
+		}
+		return a ;
+	}
+
+	public ListNode getIntersectionNode1(ListNode headA, ListNode headB) {
+		if(headA==null || headB==null){
+			return null
+		}
+		
+
+	}
 
 } 
